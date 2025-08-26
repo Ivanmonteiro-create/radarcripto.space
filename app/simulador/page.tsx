515974412e2cc2b8d18ad9d23cc5ed0f2abdda23
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-08-26 16:28:03 +0100

    Create page.tsx

---
+++ app/simulador/page.tsx
@@ -0,0 +1,11 @@
+// app/simulador/page.tsx
+import TVChart from "../../components/TVChart";
+
+export default function SimuladorPage() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Simulador de Criptomoedas</h1>
+      <TVChart />
+    </main>
+  );
+}
