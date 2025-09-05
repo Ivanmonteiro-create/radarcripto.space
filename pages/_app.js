ccddd374e22f9c5b3d0a3779b342d3f944e7069b
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-05 13:49:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,20 @@
 // pages/_app.js
-import '../styles/globals.css';
+import "@styles/globals.css";
+import { useRouter } from "next/router";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const showNav = router.pathname !== "/simulador";
+
+  return (
+    <>
+      <Header showNav={showNav} />
+      <main style={{ maxWidth: 1040, margin: "0 auto", padding: "24px 16px" }}>
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </>
+  );
 }
