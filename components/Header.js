c376f3c5a0b08a5ec8545831b90b1084d39c1597
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-05 11:29:03 +0100

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header style={{
+      background: "#111",
+      color: "#fff",
+      padding: "20px",
+      textAlign: "center"
+    }}>
+      <h1>RadarCripto.space</h1>
+      <p>Simulador de Trading</p>
+    </header>
+  )
+}
