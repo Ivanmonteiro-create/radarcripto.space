d7fde8ce089bbc26a70564dc150285771566395a
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-05 12:16:25 +0100

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer(){
   return (
     <footer className="site-footer">
-      <div className="container" style={{padding: "24px 16px", textAlign: "center", opacity: .85, fontSize: 14}}>
+      <div className="footer-inner">
         © {new Date().getFullYear()} RadarCrypto.space — Todos os direitos reservados
       </div>
     </footer>
