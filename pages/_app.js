1ab61458cb5bb9ef35a9e0657b16ba48ab5be437
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-05 14:07:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 // pages/_app.js
-import "@styles/globals.css";
+import "@styles/globals.css"; // <- arquivo CSS comum (não module)
 import { useRouter } from "next/router";
 import Header from "@components/Header";
 import Footer from "@components/Footer";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
+  // Oculta a nav apenas no /simulador
   const showNav = router.pathname !== "/simulador";
 
   return (
