df287fe5ccc7483c78f6484821d55d4ec931c7d8
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-06 11:49:35 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,14 @@
-import '@/styles/globals.css';
-import Navbar from '@components/Navbar';
-import Footer from '@components/Footer';
+// /pages/_app.js
+import "../styles/globals.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
-export default function App({ Component, pageProps }) {
-  const shell = {
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    background: 'linear-gradient(180deg,#0f1723 0%, #0a1320 100%)',
-    color: '#e6eef5',
-  };
-
+export default function MyApp({ Component, pageProps }) {
   return (
-    <div style={shell}>
+    <>
       <Navbar />
-      <main style={{ flex: 1 }}>
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
       <Footer />
-    </div>
+    </>
   );
 }
