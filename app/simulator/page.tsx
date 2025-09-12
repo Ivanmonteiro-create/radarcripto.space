a53b37de3fb21c94af4d8e0d88a62b26d5b27279
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-12 23:06:24 +0100

    Update page.tsx

--- app/simulator/page.tsx
+++ app/simulator/page.tsx
@@ -1,28 +1,11 @@
 // app/simulador/page.tsx
-'use client';
-
-import TradingViewWidget from '../../components/TradingViewWidget';
-import TradePanel from '../../components/TradePanel';
-
 export const metadata = {
   title: 'Simulador | RadarCripto',
 };
 
-export default function SimuladorPage() {
-  return (
-    <main className="mx-auto max-w-[1600px] p-4 md:p-6">
-      <div className="grid grid-cols-12 gap-4">
-        {/* Gráfico ocupa tudo que não for o painel à direita */}
-        <section className="col-span-12 lg:col-span-9 rounded-xl border border-gray-800 bg-gray-900/30 p-2">
-          {/* use o par e o intervalo que preferir */}
-          <TradingViewWidget symbol="BINANCE:BTCUSDT" interval="5" hideLegend />
-        </section>
+import SimuladorClient from './SimuladorClient';
 
-        {/* Painel de trade (mantém seu componente atual) */}
-        <aside className="col-span-12 lg:col-span-3">
-          <TradePanel />
-        </aside>
-      </div>
-    </main>
-  );
+export default function SimuladorPage() {
+  // Esta página é SERVER (sem "use client")
+  return <SimuladorClient />;
 }
