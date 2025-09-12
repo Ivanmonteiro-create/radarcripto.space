412ad34f1210546c06ecf70798496344ec7020fb
Ivanmonteiro-create <101481718+Ivanmonteiro-create@users.noreply.github.com>
2025-09-12 23:17:48 +0100

    Update and rename SimuladorClient.tsx to SimulatorClient.tsx

--- app/simulator/SimulatorClient.tsx
+++ app/simulator/SimulatorClient.tsx
@@ -1,14 +1,14 @@
 // app/simulador/SimuladorClient.tsx
 'use client';
 
-import TradingViewWidget from '../../components/TradingViewWidget';
-import TradePanel from '../../components/TradePanel';
+import TradingViewWidget from '@/components/TradingViewWidget';
+import TradePanel from '@/components/TradePanel';
 
 export default function SimuladorClient() {
   return (
     <main className="mx-auto max-w-[1600px] p-4 md:p-6">
       <div className="grid grid-cols-12 gap-4">
-        {/* Gráfico TradingView */}
+        {/* Gráfico */}
         <section className="col-span-12 lg:col-span-9 rounded-xl border border-gray-800 bg-gray-900/30 p-2">
           <TradingViewWidget symbol="BINANCE:BTCUSDT" interval="5" hideLegend />
         </section>
